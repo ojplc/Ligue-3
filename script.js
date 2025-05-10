251f3603f94b1911b4be6a15d0d24a13a38653fa
João Pedro Lopes da Cruz <103763209+ojplc@users.noreply.github.com>
2025-05-10 14:38:56 -0300

    X e O trocam de cor quando formam 3, adiciona placar e condições de vitória para o tabuleiro maior

--- script.js
+++ script.js
@@ -13,6 +13,10 @@ const square9 = document.getElementById("9");
 // pegar botao status
 const statusText = document.getElementById("status");
 
+// pegar placar
+const pontosX = document.getElementById("score-x");
+const pontosO = document.getElementById("score-o");
+
 // montar tabuleiro
 
 var tabuleiro = Array(25).fill(0);
@@ -105,6 +109,7 @@ function expandTabuleiro() {
 
 function winingStatus() {
 	winingCombinationsLevel1 = [
+		//index do tabuleiro
 		[6, 7, 8],
 		[11, 12, 13],
 		[16, 17, 18],
@@ -119,13 +124,98 @@ function winingStatus() {
 		if (
 			tabuleiro[combination[0]] === tabuleiro[combination[1]] &&
 			tabuleiro[combination[1]] === tabuleiro[combination[2]] &&
-			tabuleiro[combination[0]] !== 0
+			tabuleiro[combination[0]] !== 0 &&
+			tabuleiro[combination[0]] !== "GX" &&
+			tabuleiro[combination[0]] !== "GO"
 		) {
+			if (tabuleiro[combination[0]] == "X") {
+				pontosX.textContent = parseInt(pontosX.textContent) + 1;
+			} else if (tabuleiro[combination[0]] == "O") {
+				pontosO.textContent = parseInt(pontosO.textContent) + 1;
+			}
 			expandTabuleiro();
 
 			combination.forEach((item) => {
-				let victorySquare = document.getElementById(String(item + 1));
-				// colocar um indicador dos quadrados que ganharam
+				var victorySquare = document.getElementById(String(item + 1));
+				if (victorySquare.src.endsWith("images/X.jpg")) {
+					tabuleiro[item] = "GX";
+					victorySquare.src = "images/green_X.jpg";
+				} else if (victorySquare.src.endsWith("images/O.jpg")) {
+					tabuleiro[item] = "GO";
+					victorySquare.src = "images/green_O.jpg";
+				}
+			});
+		}
+	});
+
+	winingCombinationsLevel2 = [
+		// horiz
+		[0, 1, 2],
+		[1, 2, 3],
+		[2, 3, 4],
+		[5, 6, 7],
+		[7, 8, 9],
+		[10, 11, 12],
+		[12, 13, 14],
+		[15, 16, 17],
+		[17, 18, 19],
+		[20, 21, 22],
+		[21, 22, 23],
+		[22, 23, 24],
+		//vertical
+		[0, 5, 10],
+		[5, 10, 15],
+		[10, 15, 20],
+		[1, 6, 11],
+		[11, 16, 21],
+		[2, 7, 12],
+		[12, 17, 22],
+		[3, 8, 13],
+		[13, 18, 23],
+		[4, 9, 14],
+		[9, 14, 19],
+		[14, 19, 24],
+		//diagonal esq p dir
+		[0, 6, 12],
+		[1, 7, 13],
+		[2, 8, 14],
+		[5, 11, 17],
+		[7, 13, 19],
+		[10, 16, 22],
+		[11, 17, 23],
+		[12, 18, 24],
+		//diagonal dir p esq
+		[2, 6, 10],
+		[3, 7, 11],
+		[4, 8, 12],
+		[7, 11, 15],
+		[9, 13, 17],
+		[12, 16, 20],
+		[13, 17, 21],
+		[14, 18, 22],
+	];
+	winingCombinationsLevel2.forEach((combination) => {
+		if (
+			tabuleiro[combination[0]] !== 0 &&
+			!tabuleiro[combination[0]].startsWith("G") &&
+			tabuleiro[combination[0]] === tabuleiro[combination[1]] &&
+			tabuleiro[combination[1]] === tabuleiro[combination[2]]
+		) {
+			if (tabuleiro[combination[0]] == "X") {
+				pontosX.textContent = parseInt(pontosX.textContent) + 1;
+			} else if (tabuleiro[combination[0]] == "O") {
+				pontosO.textContent = parseInt(pontosO.textContent) + 1;
+			}
+
+			combination.forEach((item) => {
+				var victorySquare = document.getElementById(String(item + 1));
+				if (victorySquare.src.endsWith("images/X.jpg")) {
+					tabuleiro[item] = "GX";
+					victorySquare.src = "images/green_X.jpg";
+				} else if (victorySquare.src.endsWith("images/O.jpg")) {
+					tabuleiro[item] = "GO";
+					victorySquare.src = "images/green_O.jpg";
+				}
 			});
 		}
 	});
